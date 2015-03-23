9e15942430203827c07c9c1ee3f98b3549ee1554
shouko <h999342@gmail.com>
2015-03-23 16:49:44 +0800

    Fix header navbar links

--- views/header_nav_reg.php
+++ views/header_nav_reg.php
@@ -1,4 +1,4 @@
-<li><a href="./#header">首頁</a></li>
+<li><a href="./#intro">首頁</a></li>
 <li><a href="./#contact">聯絡我們</a></li>
 <li><a href="./login">登出</a></li>
 <li><a href="//facebook.com/ntuimcamp">粉絲專頁</a></li>
