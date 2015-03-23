2fe247251313c9f05844a4c6334cafe318c3291c
shouko <h999342@gmail.com>
2015-03-23 16:51:58 +0800

    Set link target of fanpage to _blank

--- views/header_nav_public.php
+++ views/header_nav_public.php
@@ -4,4 +4,4 @@
 <li><a href="./#work">課程內容</a></li>
 <li><a href="./#contact">聯絡我們</a></li>
 <li><a href="./login">立即報名</a></li>
-<li><a href="//facebook.com/ntuimcamp">粉絲專頁</a></li>
+<li><a href="//facebook.com/ntuimcamp" target="_blank">粉絲專頁</a></li>
--- views/header_nav_reg.php
+++ views/header_nav_reg.php
@@ -1,4 +1,4 @@
 <li><a href="./#intro">首頁</a></li>
 <li><a href="./#contact">聯絡我們</a></li>
 <li><a href="./login">登出</a></li>
-<li><a href="//facebook.com/ntuimcamp">粉絲專頁</a></li>
+<li><a href="//facebook.com/ntuimcamp" target="_blank">粉絲專頁</a></li>
