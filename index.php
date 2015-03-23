93bca9b25d4767327bea9c17fd096c21d182e1d4
shouko <h999342@gmail.com>
2015-03-23 16:23:45 +0800

    Add contact form handler

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+require 'includes/config.php';
 require 'vendor/autoload.php';
+use Mailgun\Mailgun;
 
 $app = new \Slim\Slim(
   array('templates.path' => './views')
@@ -21,7 +23,64 @@ $app->get('/', function() use($app) {
 
 $app->post('/contact', function() use($app) {
   require 'includes/akismet.fuspam.php';
-  use Mailgun\Mailgun;
+  $required_parameters = array("name", "email", "subject", "message");
+  $error = array();
+  foreach($required_parameters as $para){
+    if(!isset($_POST[$para])){
+      $error['title'] = "QAQ";
+      $error['message'] = "表單填寫不完整";
+    }
+  }
+  $refer_url = parse_url($_SERVER['HTTP_REFERER']);
+  if($refer_url == false || $refer_url['host'] != 'ntu.im'){
+    $error['title'] = "QAQ";
+    $error['message'] = "表單填寫不完整";
+  }
+
+  if(empty($error)){
+
+    $comment = array(
+      "blog" => "http://ntu.im/camp/",
+      "user_ip" => $_SERVER["REMOTE_ADDR"],
+      "user_agent" => $_SERVER["HTTP_USER_AGENT"],
+      "referrer" => $_SERVER["HTTP_REFERER"],
+      "permalink" => "http://ntu.im/camp/2015/",
+      "comment_type" => "",
+      "comment_author" => $_POST['name'],
+      "comment_author_email" => $_POST['email'],
+      "comment_author_url" => "",
+      "comment_content" => $_POST['subject']." ".$_POST['message']
+    );
+    $ak_result = fuspam($comment, "check-spam", AKISMET_KEY);
+
+    switch($ak_result){
+      case "true":
+        // spam
+        echo "Spam check failed";
+        break;
+      case "false":
+        //
+    //				echo "Thank you!";
+        $mg = new Mailgun(MAILGUN_KEY);
+        $domain = "ntu.shouko.tw";
+/*        $mg->sendMessage($domain, array(
+          'from'    => 'imcamp2015@ntu.shouko.tw',
+          'to'      => 'camp2015@ntu.im',
+          'subject' => $_POST['subject'],
+          'h:Reply-To' => $_POST['email'],
+          'text'    => "From: ".$_POST['name']."<".$_POST['email'].">, ".$_SERVER["REMOTE_ADDR"]."\nUser-Agent:".$_SERVER["HTTP_USER_AGENT"]."\n\n".$_POST['message']
+          )
+        );
+*/
+        $app->render('message.php', array('title' => 'Thank You!', 'message' => '感謝您的填寫，我們將盡快回覆'));
+        break;
+      default:
+        break;
+    }
+  }else{
+    $app->render('message.php', $error);
+  }
+
 });
 
 $app->get('/login', function() use($app) {
