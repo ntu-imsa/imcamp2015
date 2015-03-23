c022462098d196ce23af430c043338c72d9efe5d
shouko <h999342@gmail.com>
2015-03-23 16:23:27 +0800

    Add message view for message displaying

---
+++ views/message.php
@@ -0,0 +1,7 @@
+<section id="work" class="main style3 primary">
+  <div class="content container">
+    <header>
+      <h2><?= $title ?></h2>
+      <p><?= $content ?></p>
+    </header>
+</section>
