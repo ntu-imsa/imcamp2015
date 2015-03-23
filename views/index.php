9b74f5f7ae167bf17ea35663f3360db65aed797a
shouko <h999342@gmail.com>
2015-03-23 16:23:14 +0800

    Re-enable contact form

--- views/index.php
+++ views/index.php
@@ -81,15 +81,17 @@
 				<div class="content container">
 					<header>
 						<h2>聯絡我們</h2>
-						<p>想了解更多關於臺大資管營的訊息嗎？<br>歡迎寄信到 camp2015@ntu.im ，或<a href="//facebook.com/ntuimcamp" target="_blank">私訊粉專</a><!--，或使用以下表單聯絡我們<br>-->我們將盡快回覆 :)</p>
+						<p>想了解更多關於臺大資管營的訊息嗎？<br>歡迎寄信到camp2015@ntu.im、<a href="//facebook.com/ntuimcamp" target="_blank">私訊粉專</a>、<br>或使用以下表單聯絡我們<br>我們將盡快回覆 :)</p>
 					</header>
-<!--
 					<div class="box container 75%">
 
 							<form method="post" action="#">
 								<div class="row 50%">
 									<div class="6u"><input type="text" name="name" placeholder="姓名" /></div>
 									<div class="6u"><input type="email" name="email" placeholder="Email" /></div>
+								</div>
+								<div class="row 50%">
+									<div class="12u"><input type="text" name="subject" placeholder="主旨" /></div>
 								</div>
 								<div class="row 50%">
 									<div class="12u"><textarea name="message" placeholder="詢問或意見內容..." rows="6"></textarea></div>
@@ -102,8 +104,6 @@
 									</div>
 								</div>
 							</form>
-
 					</div>
--->
 				</div>
 			</section>
