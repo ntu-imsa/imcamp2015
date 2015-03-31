bf8d609c074b7bf0ffdaa5549afe0bdc93b47008
shouko <h999342@gmail.com>
2015-03-31 21:54:34 +0800

    Update nav bar

--- views/header_nav_public.php
+++ views/header_nav_public.php
@@ -1,7 +1,5 @@
 <li><a href="./#">首頁</a></li>
 <li><a href="./#one">營隊簡介</a></li>
-<li><a href="./#two">報名資訊</a></li>
 <li><a href="./#work">課程內容</a></li>
-<li><a href="./#contact">聯絡我們</a></li>
 <li><a href="./register">立即報名</a></li>
-<li><a href="//facebook.com/ntuimcamp" target="_blank">粉絲專頁</a></li>
+<li><a href="./#contact">聯絡我們</a></li>
